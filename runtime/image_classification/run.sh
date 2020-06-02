67da7bab57c1531126e36c6343714a3f5675e099
niranda <niranda.perera@gmail.com>
2020-06-02 16:43:56 -0400

    updating sh scripts

--- runtime/image_classification/run.sh
+++ runtime/image_classification/run.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=4,5,6,7
 
-# MODULE="models.resnet50.gpus=4_straight"
-MODULE="models.alexnet.gpus=4_straight"
+MODEL="resnet50"
+#MODEL="alexnet"
 
-# CONFIG="models/resnet50/gpus=4_straight/mp_conf.json"
-CONFIG="models/alexnet/gpus=4_straight/mp_conf.json"
+CONF="4_straight"
+
+MODULE="models.$MODEL.gpus=$CONF"
+CONFIG="models/$MODEL/gpus=$CONF/mp_conf.json"
 
 DATA="/N/u2/d/dnperera/data/imagenet-mini/"
 # DATA="/N/u2/d/dnperera/data/ILSVRC/Data/CLS-LOC/"
 
 LOGS_DIR="logs/$MODULE"
-echo "mking dir $LOGS_DIR"
+echo "making dir $LOGS_DIR"
 mkdir -p "$LOGS_DIR"
-rm "$LOGS_DIR"/*
+rm -f "$LOGS_DIR"/*
 
 command() {
-        echo "### batch size $1 rank $2 start"
-        python main_with_runtime.py --module "$MODULE" -b "$1" --data_dir $DATA --rank "$2" --local_rank "$2" --master_addr localhost --config_path "$CONFIG" --distributed_backend gloo --epochs 3 -v 1 -j 10
-        echo "### batch size $1 rank $2 end"
+  echo "### batch size $1 rank $2 start"
+  python main_with_runtime.py --module "$MODULE" -b "$1" --data_dir $DATA --rank "$2" --local_rank "$2" --master_addr localhost --config_path "$CONFIG" --distributed_backend gloo --epochs 3 -v 1 -j 10
+  echo "### batch size $1 rank $2 end"
 }
 
-for b in 64 128 256 512
-do
-        echo "batch $b start"
-        for r in 0 1 2 3
-        do
-                command $b $r &> "$LOGS_DIR"/"$b"_"$r".log &
-        done
-        wait
-        echo "batch $b done!"
-        echo "-----------------------------------------------------------"
+for b in 64 128 256 512; do
+  echo "batch $b start"
+  for r in 0 1 2 3; do
+    command $b $r &>"$LOGS_DIR"/"$b"_"$r".log &
+  done
+  wait
+  echo "batch $b done!"
+  echo "-----------------------------------------------------------"
 done
 
 echo "zipping $LOGS_DIR"
-cd $LOGS_DIR && tar czf ../../logs_"$MODULE".tar *.log && cd - 
+cd $LOGS_DIR && tar czf ../../logs_"$MODULE".tar *.log && cd -
--- runtime/image_classification/run_1.sh
+++ runtime/image_classification/run_1.sh
@@ -1,25 +1,39 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=4
 
-LOGS_DIR=logs_1
-mkdir -p $LOGS_DIR
-rm -rf $LOGS_DIR/*
+MODEL="resnet50"
+#MODEL="alexnet"
+
+CONF="4_straight"
+
+MODULE="models.$MODEL.gpus=$CONF"
+CONFIG="models/$MODEL/gpus=$CONF/1_conf.json"
+
+DATA="/N/u2/d/dnperera/data/imagenet-mini/"
+# DATA="/N/u2/d/dnperera/data/ILSVRC/Data/CLS-LOC/"
+
+LOGS_DIR="logs_1/$MODULE"
+echo "making dir $LOGS_DIR"
+mkdir -p "$LOGS_DIR"
+rm -f "$LOGS_DIR"/*
 
 command() {
-	echo "### batch size $1 rank $2 start"
-	python main_with_runtime.py --module models.alexnet.gpus=4_straight -b "$1" --data_dir /N/u2/d/dnperera/data/imagenet-mini/ --rank "$2" --local_rank "$2" --master_addr localhost --config_path models/alexnet/gpus=4_straight/1_conf.json --distributed_backend gloo --epochs 3 -v 1  -j 10
-	echo "### batch size $1 rank $2 end"
+  echo "### batch size $1 rank $2 start"
+  python main_with_runtime.py --module "$MODULE" -b "$1" --data_dir "$DATA" --rank "$2" --local_rank "$2" --master_addr localhost --config_path "$CONFIG" --distributed_backend gloo --epochs 3 -v 1 -j 10
+  echo "### batch size $1 rank $2 end"
 }
 
-for b in 64 128 256 512
-do
-	echo "batch $b start"
-#	for r in 0 
-#	do
-	r=0
-	command $b $r &> "$LOGS_DIR"/"$b"_"$r".log 
-#	done
-	wait
-	echo "batch $b done!"
-	echo "-----------------------------------------------------------"
+for b in 64 128 256 512; do
+  echo "batch $b start"
+  #	for r in 0
+  #	do
+  r=0
+  command $b $r &>"$LOGS_DIR"/"$b"_"$r".log
+  #	done
+  wait
+  echo "batch $b done!"
+  echo "-----------------------------------------------------------"
 done
+
+echo "zipping $LOGS_DIR"
+cd $LOGS_DIR && tar czf ../../logs_1_"$MODULE".tar *.log && cd - || return
