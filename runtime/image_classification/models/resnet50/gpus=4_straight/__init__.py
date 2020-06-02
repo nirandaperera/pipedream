dfa267f47f64ba91c3ac51816bb5c4bfe58c3a7f
niranda <niranda.perera@gmail.com>
2020-06-02 15:06:00 -0400

    adding resnet 4 straight

--- runtime/image_classification/models/resnet50/gpus=4_straight/__init__.py
+++ runtime/image_classification/models/resnet50/gpus=4_straight/__init__.py
@@ -1,11 +1,11 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
+from .resnet50 import ResNet50Split
 from .stage0 import Stage0
 from .stage1 import Stage1
 from .stage2 import Stage2
 from .stage3 import Stage3
-from .resnet50 import ResNet50Partitioned
 
 def arch():
     return "resnet50"
@@ -14,10 +14,10 @@ def model(criterion):
     return [
         (Stage0(), ["input0"], ["out0", "out1"]),
         (Stage1(), ["out0", "out1"], ["out3", "out2"]),
-        (Stage2(), ["out3", "out2"], ["out4", "out5"]),
-        (Stage3(), ["out4", "out5"], ["out6"]),
+        (Stage2(), ["out3", "out2"], ["out5", "out4"]),
+        (Stage3(), ["out5", "out4"], ["out6"]),
         (criterion, ["out6"], ["loss"])
     ]
 
 def full_model():
-    return ResNet50Partitioned()
+    return ResNet50Split()
--- runtime/image_classification/models/resnet50/gpus=4_straight/resnet50.py
+++ runtime/image_classification/models/resnet50/gpus=4_straight/resnet50.py
@@ -7,21 +7,21 @@ from .stage1 import Stage1
 from .stage2 import Stage2
 from .stage3 import Stage3
 
-class ResNet50Partitioned(torch.nn.Module):
+
+class ResNet50Split(torch.nn.Module):
     def __init__(self):
-        super(ResNet50Partitioned, self).__init__()
+        super(ResNet50Split, self).__init__()
         self.stage0 = Stage0()
         self.stage1 = Stage1()
         self.stage2 = Stage2()
         self.stage3 = Stage3()
-
         self._initialize_weights()
 
     def forward(self, input0):
         (out0, out1) = self.stage0(input0)
         (out3, out2) = self.stage1(out0, out1)
-        (out4, out5) = self.stage2(out3, out2)
-        out6 = self.stage3(out4, out5)
+        (out5, out4) = self.stage2(out3, out2)
+        out6 = self.stage3(out5, out4)
         return out6
 
     def _initialize_weights(self):
