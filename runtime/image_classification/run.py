d2484ecc72a0be2ce8ea6426101b0df2ad802be7
niranda <niranda.perera@gmail.com>
2020-06-05 18:08:52 -0400

    adding run.py

---
+++ runtime/image_classification/run.py
@@ -0,0 +1,24 @@
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('-m', dest='models', type=str, nargs='+',
+                    help='models', required=True)
+parser.add_argument('-b', dest='batches', type=int, nargs='+',
+                    help='num batches', required=True)
+parser.add_argument('-c', dest='configs', type=str, nargs='+',
+                    help='configs', default=['4_straight'])
+parser.add_argument('-j', dest='jsons', type=str, nargs='+',
+                    help='jsons and gpus', default=['mp_config'])
+
+args = parser.parse_args()
+args = vars(args)
+
+for m in args["models"]:
+    for c in args["configs"]:
+        for j in args["jsons"]:
+            for b in args["batches"]:
+                ranks = 1 if '1_conf' in j else c.split('_')[0]
+                run_args = f" {m} {c} {j} {ranks} {b}"
+                print(f"Running {run_args}")
+                os.system(f"./run.sh {run_args}")
--- runtime/image_classification/run.sh
+++ runtime/image_classification/run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=4,5,6,7
 
+DATA="/N/u2/d/dnperera/data/imagenet-mini/"
+
 MODEL="$1"
 CONF="$2"
 JSON="$3"
@@ -10,10 +12,7 @@ BATCH_SIZE="$5"
 MODULE="models.$BATCH_SIZE.$MODEL.gpus=$CONF"
 CONFIG="models/$BATCH_SIZE/$MODEL/gpus=$CONF/$JSON.json"
 
-echo "module $MODULE config $CONFIG ranks $RANKS"
-
-DATA="/N/u2/d/dnperera/data/imagenet-mini/"
-# DATA="/N/u2/d/dnperera/data/ILSVRC/Data/CLS-LOC/"
+echo "@@@ module $MODULE config $CONFIG ranks $RANKS batch size $BATCH_SIZE"
 
 LOGS_DIR="logs/${BATCH_SIZE}_${MODEL}_${CONF}_${JSON}"
 echo "making dir $LOGS_DIR"
@@ -22,20 +21,17 @@ rm -f "$LOGS_DIR"/*
 
 command() {
   echo "### batch size $1 rank $2 start"
-  python main_with_runtime.py --module "$MODULE" -b "$1" --data_dir $DATA --rank "$2" --local_rank "$2" --master_addr localhost --config_path "$CONFIG" --distributed_backend gloo --epochs 3 -v 1 -j 10
+  python main_with_runtime.py --module "$MODULE" -b "$1" --data_dir $DATA --rank "$2" --local_rank "$2" \
+    --master_addr localhost --config_path "$CONFIG" --distributed_backend gloo --epochs 3 -v 1 -j 10
   echo "### batch size $1 rank $2 end"
 }
 
-#for b in 64 128 256 512; do
-for b in $BATCH_SIZE; do
-  echo "batch $b start"
-  for ((r = 0; r < RANKS; r++)); do
-    command $b $r &>"$LOGS_DIR"/"$b"_"$r".log &
-  done
-  wait
-  echo "batch $b done!"
-  echo "-----------------------------------------------------------"
+for ((r = 0; r < RANKS; r++)); do
+  command "$BATCH_SIZE" $r &>"$LOGS_DIR"/"$BATCH_SIZE"_"$r".log &
 done
+wait
 
 echo "zipping $LOGS_DIR"
 cd "$LOGS_DIR" && tar czf ../../"${MODEL}_${CONF}_${JSON}".tar ./*.log && cd - || return
+
+echo "@@@ module $MODULE config $CONFIG ranks $RANKS batch size $BATCH_SIZE DONE!"
