692e22f753453fb24f4265b89c103f5a1efb84e0
niranda <niranda.perera@gmail.com>
2020-06-05 14:42:45 -0400

    revert run changes

--- runtime/image_classification/run.sh
+++ runtime/image_classification/run.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=4,5,6,7
 
-DATA="/N/u2/d/dnperera/data/imagenet-mini/"
-
-
-invoke_main() {
-  local MODULE=$3
-  local CONFIG=$4
-  echo "### batch size $1 rank $2 config $3 start "
-  python main_with_runtime.py --module "$MODULE" -b "$1" --data_dir $DATA --rank "$2" --local_rank "$2" --master_addr localhost --config_path "$CONFIG" --distributed_backend gloo --epochs 3 -v 1 -j 10
-  echo "### batch size $1 rank $2 end"
-}
-
-
 MODEL="$1"
 CONF="$2"
 JSON="$3"
@@ -24,6 +12,7 @@ CONFIG="models/$BATCH_SIZE/$MODEL/gpus=$CONF/$JSON.json"
 
 echo "module $MODULE config $CONFIG ranks $RANKS"
 
+DATA="/N/u2/d/dnperera/data/imagenet-mini/"
 # DATA="/N/u2/d/dnperera/data/ILSVRC/Data/CLS-LOC/"
 
 LOGS_DIR="logs/${BATCH_SIZE}_${MODEL}_${CONF}_${JSON}"
@@ -31,13 +20,17 @@ echo "making dir $LOGS_DIR"
 mkdir -p "$LOGS_DIR"
 rm -f "$LOGS_DIR"/*
 
-
+command() {
+  echo "### batch size $1 rank $2 start"
+  python main_with_runtime.py --module "$MODULE" -b "$1" --data_dir $DATA --rank "$2" --local_rank "$2" --master_addr localhost --config_path "$CONFIG" --distributed_backend gloo --epochs 3 -v 1 -j 10
+  echo "### batch size $1 rank $2 end"
+}
 
 #for b in 64 128 256 512; do
 for b in $BATCH_SIZE; do
   echo "batch $b start"
   for ((r = 0; r < RANKS; r++)); do
-    invoke_main $b $r &>"$LOGS_DIR"/"$b"_"$r".log &
+    command $b $r &>"$LOGS_DIR"/"$b"_"$r".log &
   done
   wait
   echo "batch $b done!"
